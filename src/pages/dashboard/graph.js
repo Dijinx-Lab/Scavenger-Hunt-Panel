052eeb71a0daffefdf0203234f7882aa31c0265f
Basil <basiljamil1122@gmail.com>
2024-05-23 23:20:21 +0500

    pushing

--- src/pages/dashboard/graph.js
+++ src/pages/dashboard/graph.js
@@ -132,14 +132,27 @@ const LineChart = () => {
 
           {/* <div className='fixed top-[40.5%] ml-[26%]'>  */}
           
-          
-        <select className="border text-black border-gray-300 rounded-sm px-2 py-2 focus:outline-none bg-gray-300 focus:border-none">
-          <option value="daily">Daily</option>
+          {/* <select className="border text-black border-gray-300 rounded-sm px-2 py-2 focus:outline-none bg-gray-300 focus:border-none">
           <option value="monthly">Monthly</option>
-          <option value="yearly">Yearly</option>
-        </select>
+          <option value="weakly">Weekly</option>
+          <option value="daily">Daily</option>
+        </select> */}
+   
         {/* </div> */}
         </div>
+        
+        <div className="relative inline-block  w-[9rem]">
+      <select className="border text-black border-gray-300  rounded-sm px-2 py-2 focus:outline-none bg-gray-300 appearance-none w-full">
+        <option value="monthly">Monthly</option>
+        <option value="weakly">Weekly</option>
+        <option value="daily">Daily</option>
+      </select>
+      <div className="pointer-events-none  absolute inset-y-0 right-0 flex items-center pr-1 text-black">
+        <svg className="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+        </svg>
+      </div>
+    </div>
       </div>
       <div className="pt-6 px-2    pb-0">
         <div className="overflow-x-auto overflow-y-hidden"  id="line-chart"></div>
